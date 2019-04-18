f359a137395deed06597cbf34c888cb453db5f51
Matthew Craig <taggedzi.mpc@gmail.com>
2019-04-18 06:22:42 -0400

    Added comments

--- src/Address.py
+++ src/Address.py
@@ -4,92 +4,171 @@ import json
 
 
 class Address(object):
+    """
+    A simple object used in learning python AND to manage addresses in a USA address format.
+    """
 
     @property
     def name(self):
+        """
+        :return: String Name
+        """
         return self.__name
 
     @name.setter
     def name(self, value=None):
+        """
+        Collect a value and set the address name to that value
+        :param value: String
+        :return: void
+        """
         if value is None:
             value = ''
         self.__name = value
 
     @name.deleter
     def name(self):
+        """
+        Cleanup name
+        :return: void
+        """
         del self.__name
 
     @property
     def street(self):
+        """
+        :return: String Street
+        """
         return self.__street
 
     @street.setter
     def street(self, value=None):
+        """
+        Collect a value for street and store it
+        :param value: String Street
+        :return: void
+        """
         if value is None:
             value = ''
         self.__street = value
 
     @street.deleter
     def street(self):
+        """
+        Cleanup the street value
+        :return: void
+        """
         del self.__street
 
     @property
     def street2(self):
+        """
+        :return: String Street 2
+        """
         return self.__street2
 
     @street2.setter
     def street2(self, value=None):
+        """
+        Collect a value for street 2 and store it
+        :param value: String Street 2
+        :return: void
+        """
         if value is None:
             value = ''
         self.__street2 = value
 
     @street2.deleter
     def street2(self):
+        """
+        Cleanup after street 2
+        :return: void
+        """
         del self.__street2
 
     @property
     def city(self):
+        """
+        :return: String City
+        """
         return self.__city
 
     @city.setter
     def city(self, value=None):
+        """
+        Collect a value for city and store it
+        :param value: String City
+        :return: void
+        """
         if value is None:
             value = ''
         self.__city = value
 
     @city.deleter
     def city(self):
+        """
+        Cleanup city data
+        :return: void
+        """
         del self.__city
 
     @property
     def state(self):
+        """
+        :return: String State
+        """
         return self.__state
 
     @state.setter
     def state(self, value=None):
+        """
+        Collect a state and store it
+        :param value: String state
+        :return: void
+        """
         if value is None:
             value = ''
         self.__state = value
 
     @state.deleter
     def state(self):
+        """
+        Cleanup state dat
+        :return: void
+        """
         del self.__state
 
     @property
     def zip_code(self):
+        """
+        :return: String Zip code
+        """
         return self.__zip_code
 
     @zip_code.setter
     def zip_code(self, value=None):
+        """
+        Collect a zip code and store it
+        :param value: String zip code
+        :return: void
+        """
         if value is None:
             value = ''
         self.__zip_code = value
 
     @zip_code.deleter
     def zip_code(self):
+        """
+        Cleanup zip code data
+        :return: void
+        """
         del self.__zip_code
 
     def export_json_str(self):
+        """
+        Export an address object as a JSON String
+        :return: String JSON string representing an address
+        """
         output = {
             "name": self.name,
             "street": self.street,
@@ -101,6 +180,11 @@ class Address(object):
         return json.dumps(output)
 
     def import_json_str(self, in_address=None):
+        """
+        Take a JSON encoded string and import it into an Address object
+        :param in_address: String[JSON]
+        :return:void
+        """
         temp = json.loads(in_address)
         self.name = temp.get("name", None)
         self.street = temp.get("street", None)
@@ -110,9 +194,15 @@ class Address(object):
         self.zip_code = temp.get("zip_code", None)
 
     def __repr__(self):
+        """
+        :return: String representation of an address object
+        """
         return '<Address name="{0.name}" street="{0.street}">'.format(self)
 
     def __str__(self):
+        """
+        :return: String A user friendly string represntation of an address object.
+        """
         output = '-' * 80 + "\n"
         output += "{0.name}\n"
         output += "{0.street}\n"
@@ -122,20 +212,44 @@ class Address(object):
         return output.format(self)
 
     def __eq__(self, other):
+        """
+        Compare all features of an address to determine if they are the same
+        :param other: Address an address type object to compare with.
+        :return: Bool
+        """
         current = (self.name, self.street, self.street2, self.city, self.state, self.zip_code)
         other_val = (other.name, other.street, other.street2, other.city, other.state, other.zip_code)
         return current == other_val
 
     def __ne__(self, other):
+        """
+        Determine if two address type objects are NOT equal
+        :param other: Object to compare
+        :return: Bool
+        """
         return not (self == other)
 
     def __lt__(self, other):
+        """
+        Less than comparison of an address (primarily used for sorting by address name)
+        :param other: Address an Address object to compare with
+        :return: Bool
+        """
         return self.name.lower() < other.name.lower()
 
     def __gt__(self, other):
+        """
+        Greater than comparison of an address (primarily used for sorting by address name)
+        :param other: Address an Address object to compare with
+        :return: Bool
+        """
         return self.name.lower() > other.name.lower()
 
     def __del__(self):
+        """
+        Self cleanup and garbage collection
+        :return:
+        """
         del self.name
         del self.street
         del self.street2
@@ -144,6 +258,9 @@ class Address(object):
         del self.zip_code
 
     def __init__(self):
+        """
+        Initialization
+        """
         self.name = None
         self.street = None
         self.street2 = None
