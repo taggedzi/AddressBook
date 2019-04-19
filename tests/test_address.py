45f8385507ad2e9e3123bb53765d9d38e155da11
Matthew Craig <taggedzi.mpc@gmail.com>
2019-04-19 04:34:51 -0400

    More Unit tests

--- tests/test_address.py
+++ tests/test_address.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
 from src.Address import Address
+
+SAMPLE_NAME = "Name"
+SAMPLE_STREET = "Street"
+SAMPLE_STREET2 = "Street2"
+SAMPLE_CITY = "City"
+SAMPLE_STATE = "Stage"
+STATE_ZIP_CODE = "Zip_Code"
 
 
 def test_init_address_pass():
@@ -115,74 +123,167 @@ def test_zip_code_value():
     assert_zip_code_value('random_string', 'random_string')
 
 
+def build_json(name="Test", street="Street", street2="Street2", city="City", state="State", zip_code="Zip_Code"):
+    return json.dumps({
+        "name": name,
+        "street": street,
+        "street2": street2,
+        "city": city,
+        "state": state,
+        "zip_code": zip_code
+    })
+
+
+def build_object(name="Test", street="Street", street2="Street2", city="City", state="State", zip_code="Zip_Code"):
+    output = Address()
+    output.name = name
+    output.street = street
+    output.street2 = street2
+    output.city = city
+    output.state = state
+    output.zip_code = zip_code
+    return output
+
+
 def test_export_json_str():
-    test = Address()
-    test.name = "Test"
-    test.street = "Street"
-    test.street2 = "Street2"
-    test.city = "City"
-    test.state = "State"
-    test.zip_code = "Zip_Code"
-    result = test.export_json_str()
-    expected = '{"name": "Test", "street": "Street", "street2": "Street2", "city": "City", "state": "State", ' \
-               '"zip_code": "Zip_Code"}'
-    assert result == expected
+    test = build_object()
+    assert test.export_json_str() == build_json()
 
 
-def test_export_json_str_fail():
-    test = Address()
-    test.name = "Test1"
-    test.street = "Street"
-    test.street2 = "Street2"
-    test.city = "City"
-    test.state = "State"
-    test.zip_code = "Zip_Code"
-    result = test.export_json_str()
-    expected = '{"name": "Test", "street": "Street", "street2": "Street2", "city": "City", "state": "State", ' \
-               '"zip_code": "Zip_Code"}'
-    assert result != expected
+def test_export_json_str_name_fail():
+    test = build_object()
+    test.name = "Different"
+    assert test.export_json_str() != build_json()
+
+
+def test_export_json_str_street_fail():
+    test = build_object()
+    test.street = "Different"
+    assert test.export_json_str() != build_json()
+
+
+def test_export_json_str_street2_fail():
+    test = build_object()
+    test.street2 = "Different"
+    assert test.export_json_str() != build_json()
+
+
+def test_export_json_str_city_fail():
+    test = build_object()
+    test.city = "Different"
+    assert test.export_json_str() != build_json()
+
+
+def test_export_json_str_state_fail():
+    test = build_object()
+    test.state = "Different"
+    assert test.export_json_str() != build_json()
+
+
+def test_export_json_str_zip_code_fail():
+    test = build_object()
+    test.zip_code = "Different"
+    assert test.export_json_str() != build_json()
 
 
 def test_import_json_str():
-    sample = '{"name": "Test", "street": "Street", "street2": "Street2", "city": "City", "state": "State", ' \
-             '"zip_code": "Zip_Code"}'
     test = Address()
-    test.import_json_str(sample)
-    expected = Address()
-    expected.name = "Test"
-    expected.street = "Street"
-    expected.street2 = "Street2"
-    expected.city = "City"
-    expected.state = "State"
-    expected.zip_code = "Zip_Code"
-    assert test == expected
+    test.import_json_str(build_json())
+    assert test == build_object()
+
+
+def test_import_json_str_name_fail():
+    test = Address()
+    test.import_json_str(build_json(name="Different"))
+    assert test != build_object()
+
+
+def test_import_json_str_street_fail():
+    test = Address()
+    test.import_json_str(build_json(street="Different"))
+    assert test != build_object()
+
+
+def test_import_json_str_street2_fail():
+    test = Address()
+    test.import_json_str(build_json(street2="Different"))
+    assert test != build_object()
+
+
+def test_import_json_str_city_fail():
+    test = Address()
+    test.import_json_str(build_json(city="Different"))
+    assert test != build_object()
+
+
+def test_import_json_str_state_fail():
+    test = Address()
+    test.import_json_str(build_json(state="Different"))
+    assert test != build_object()
+
+
+def test_import_json_str_zip_code_fail():
+    test = Address()
+    test.import_json_str(build_json(zip_code="Different"))
+    assert test != build_object()
 
 
 def test_eq():
-    sample = '{"name": "Test", "street": "Street", "street2": "Street2", "city": "City", "state": "State", ' \
-             '"zip_code": "Zip_Code"}'
-    test = Address()
-    test.import_json_str(sample)
-    expected = Address()
-    expected.name = "Test"
-    expected.street = "Street"
-    expected.street2 = "Street2"
-    expected.city = "City"
-    expected.state = "State"
-    expected.zip_code = "Zip_Code"
-    assert test == expected
+    test1 = build_object()
+    test2 = build_object()
+    assert test1 == test2
+
+
+def test_eq_name():
+    test1 = build_object()
+    test2 = build_object()
+    test1.name = "Test1"
+    test2.name = "Test1"
+    assert test1 == test2
+
+
+def test_eq_street():
+    test1 = build_object()
+    test2 = build_object()
+    test1.street = "Test1"
+    test2.street = "Test1"
+    assert test1 == test2
+
+
+def test_eq_street2():
+    test1 = build_object()
+    test2 = build_object()
+    test1.street2 = "Test1"
+    test2.street2 = "Test1"
+    assert test1 == test2
+
+
+def test_eq_city():
+    test1 = build_object()
+    test2 = build_object()
+    test1.city = "Test1"
+    test2.city = "Test1"
+    assert test1 == test2
+
+
+def test_eq_state():
+    test1 = build_object()
+    test2 = build_object()
+    test1.state = "Test1"
+    test2.state = "Test1"
+    assert test1 == test2
+
+
+def test_eq_zip_code():
+    test1 = build_object()
+    test2 = build_object()
+    test1.zip_code = "Test1"
+    test2.zip_code = "Test1"
+    assert test1 == test2
 
 
 def test_ne():
-    sample = '{"name": "Test1", "street": "Street", "street2": "Street2", "city": "City", "state": "State", ' \
-             '"zip_code": "Zip_Code"}'
-    test = Address()
-    test.import_json_str(sample)
-    expected = Address()
-    expected.name = "Test"
-    expected.street = "Street"
-    expected.street2 = "Street2"
-    expected.city = "City"
-    expected.state = "State"
-    expected.zip_code = "Zip_Code"
-    assert test != expected
+    test1 = build_object()
+    test2 = build_object()
+    assert test1 == test2
+
